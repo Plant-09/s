c206f0761c0dba368ed677f6a6e61e68674b63ca
Plant-09 <syedomar.0511@gmail.com>
2026-09-22 21:17:52 +0530

    Update problem04.c

---
+++ set01/problem04.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <math.h>
+
+float input()
+{
+  float s;
+  printf("Enter the number\n");
+  scanf("%f",&s);
+  return s;
+}
+
+float squareroot(float s)
+{
+  float guess = s/2;
+  float next_guess =  (guess + s/guess)/2;
+ 
+  while( fabs(guess - next_guess) > 0.00001 )
+  {
+    guess = next_guess;
+    next_guess = (guess + s/guess)/2;
+  }
+  return guess;
+}
+
+void output(float s, float root)
+{
+  printf("The square root of %f is %f\n",s,root);
+}
+
+int main()
+{
+  float s,root;
+  s=input();
+  root = squareroot(s);
+  output(s,root);
+}
